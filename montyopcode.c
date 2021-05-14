7ccb99db4591340fcc495d0916f8ba5cdb95c6ba
sergiovelasquez18 <2458@holbertonschool.com>
2021-05-13 22:44:05 -0500

    monty

--- montyopcode.c
+++ montyopcode.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 /**
- * push - add a new node at the beginnig of a dlistint_t list
- * @head: pointer to a pointer to struct of type stack_t
+ * push - adds a new node at the beginning of a dlistint_t list
+ * @head: pointer to a pointer to a struct of type stack_t
  * @line_number: line_number of instruction
  * Return: address of new element
  */
@@ -15,7 +15,7 @@ void push(stack_t **head, unsigned int line_number)
 	if (helpy.queueflag == 1)
 		result = queuepush(&helpy.head, helpy.line_number);
 	if (result == 0)
-	return;
+		return;
 	if (!head)
 	{
 		free_everything();
@@ -25,7 +25,7 @@ void push(stack_t **head, unsigned int line_number)
 	if (newnode == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
-			free_everything();
+		free_everything();
 		exit(EXIT_FAILURE);
 	}
 	num = atoi(helpy.token2);
@@ -43,9 +43,9 @@ void push(stack_t **head, unsigned int line_number)
 }
 
 /**
- * pall - prints all the the elements of a stack_t list
- * @head: pointer to struct of type stack_t
- * @line_number: line_number of instruction
+ * pall - prints all the elements of a stack_t list
+ * @head: pointer to a struct of type stack_t
+ * @line_number: line number of instruction
  * Return: number of nodes
  */
 void pall(stack_t **head, unsigned int line_number)
@@ -60,6 +60,10 @@ void pall(stack_t **head, unsigned int line_number)
 	}
 	*head = tmp;
 }
+/**
+ * free_dlistint - free a dlistint_t list
+ * @head: pointer to stack_t struct
+ */
 void free_dlistint(stack_t *head)
 {
 	stack_t *tmp = NULL;
@@ -70,4 +74,49 @@ void free_dlistint(stack_t *head)
 		free(head);
 		head = tmp;
 	}
+}
+/**
+ * pint - prints first element of a dlistint_t list
+ * @head: pointer to a pointer to a struct of type stack_t
+ * @line_number: line number of instruction
+ * Return: void
+ */
+void pint(stack_t **head, unsigned int line_number)
+{
+	if (*head)
+		printf("%d\n", (*head)->n);
+	else
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		free_everything();
+		exit(EXIT_FAILURE);
+	}
+}
+/**
+ * sw - swaps the top two elements of the stack
+ * @head: pointer to a pointer to a struct of type stack_t
+ * @line_number: line number of instruction
+ * Return: void
+ */
+void sw(stack_t **head, unsigned int line_number)
+{
+	int tmp;
+	stack_t *current = *head;
+	int counter = 0;
+
+	while (current)
+	{
+		counter++;
+		current = current->next;
+	}
+	if (counter < 2)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n",
+			line_number);
+		free_everything();
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*head)->n;
+	(*head)->n = (*head)->next->n;
+	(*head)->next->n = tmp;
 }
